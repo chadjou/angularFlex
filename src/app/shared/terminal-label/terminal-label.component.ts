dfd5e07735a908cb93d6a8dc64d65c54c31c406f
Unknown <vladislav.kov@gmail.com>
2018-10-27 09:17:15 +0300

    moving html to terminal label

--- src/app/shared/terminal-label/terminal-label.component.ts
+++ src/app/shared/terminal-label/terminal-label.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-terminal-label',
-  templateUrl: './terminal-label.component.html',
-  styleUrls: ['./terminal-label.component.css']
+  selector: "app-terminal-label",
+  templateUrl: "./terminal-label.component.html",
+  styleUrls: ["./terminal-label.component.css"]
 })
 export class TerminalLabelComponent implements OnInit {
+  @Input()
+  terminal: any;
+  @Input()
+  seller: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
